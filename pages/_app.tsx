bddaef6e04a02e63955e0271a608b2954cb65c67
daitennn <baseball.11.daisuke@gmail.com>
2023-03-24 17:35:12 +0900

    app.tsxの修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,9 @@ import type { AppProps } from 'next/app'
 //   return <Component {...pageProps} />
 // }
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }: AppProps) => (
   /* eslint-disable react/jsx-props-no-spreading */
   <Component {...pageProps} />
-}
+);
 
 export default App;
