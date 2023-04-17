279542b83109a06bcf0ac0444758c5bf542b7193
yziori <ms6rx7861@gmail.com>
2023-04-18 00:46:45 +0900

    docs : 📝 chipImage.tsにドキュメント追加

--- games/common/Factories/chipImage.ts
+++ games/common/Factories/chipImage.ts
@@ -11,6 +11,18 @@ export default class Chip extends Phaser.GameObjects.Image {
 
   #text: Text;
 
+  /**
+   * Chipオブジェクトを生成する
+   * @remarks
+   * PhaserのImageオブジェクトを拡張している
+   * @param scene - Chipオブジェクトを追加するPhaser.Sceneオブジェクト
+   * @param x - Chipオブジェクトのx座標
+   * @param y - Chipオブジェクトのy座標
+   * @param texture - チップのテクスチャ名
+   * @param key - チップのキー名
+   * @param value - チップの額面の値
+   * @param textStyle - チップのテキストスタイル
+   */
   constructor(
     scene: Phaser.Scene,
     x: number,
@@ -48,6 +60,14 @@ export default class Chip extends Phaser.GameObjects.Image {
     this.setHoverHandler();
   }
 
+  /**
+   * Chipをマウスオーバーしたときの挙動を設定する
+   *
+   * @remarks
+   * Chipをマウスオーバーしたとき、Chipのサイズを1.2倍に拡大する
+   *
+   * @returns なし。
+   */
   private setHoverHandler(): void {
     this.on(
       'pointerover',
@@ -65,24 +85,48 @@ export default class Chip extends Phaser.GameObjects.Image {
     );
   }
 
+  /**
+   * Chipの額面の値を取得する
+   * @returns Chipの額面の値
+   */
   getChipValue(): number {
     return this.data.get('value');
   }
 
+  /**
+   * Chipをクリックしたときの挙動を設定する
+   * @param pushHandler Chipをクリックしたときに実行する関数
+   */
   setClickHandler(pushHandler: () => void): void {
     this.on('pointerdown', pushHandler, this);
   }
 
+  /**
+   * Chipオブジェクトを破棄する
+   * @remarks
+   * 同時にテキストオブジェクトも破棄する
+   */
   destroy(): void {
     this.#text.destroy();
     super.destroy();
   }
 
+  /**
+   * Chipオブジェクトのx座標を設定する
+   * @remarks
+   * Chipオブジェクトのx座標設定後、テキストをChipの中央に配置
+   * @param x Chipオブジェクトのx座標
+   */
   setX(x: number): any {
     super.setX(x);
     Phaser.Display.Align.In.Center(this.#text, this);
   }
 
+  /**
+   * Chipオブジェクトをアニメーションで移動させる。
+   * @param toX 移動先のx座標
+   * @param toY 移動先のy座標
+   */
   playMoveTween(toX: number, toY: number): void {
     this.scene.tweens.add({
       targets: this,
