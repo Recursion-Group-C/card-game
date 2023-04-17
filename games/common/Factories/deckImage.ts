b590ad602fece1673b3e1a2f7a630f3e8deccc43
yziori <ms6rx7861@gmail.com>
2023-04-18 00:47:38 +0900

    docs : 📝 deckImage.tsにドキュメント追加

--- games/common/Factories/deckImage.ts
+++ games/common/Factories/deckImage.ts
@@ -27,6 +27,20 @@ const RANK_CHOICES = [
 export default class Deck {
   cardList: Array<Card> = [];
 
+  /**
+   * Deckクラスのコンストラクタ
+   *
+   * @remarks
+   * カードのリストを初期化する
+   *
+   * @param scene - このDeckクラスを生成するPhaser.Sceneインスタンス。
+   * @param x - カードのx座標。
+   * @param y - カードのy座標。
+   * @param suitChoices - デッキに含めるマークの配列。
+   * @param rankChoices - デッキに含めるランクの配列。
+   *
+   * @returns Deckクラスのインスタンス。
+   */
   constructor(
     scene: Phaser.Scene,
     x: number,
@@ -50,6 +64,9 @@ export default class Deck {
     }
   }
 
+  /**
+   * デッキをシャッフルする。
+   */
   shuffle(): void {
     for (let i = 0; i < this.cardList.length; i += 1) {
       const randomIndex: number = Math.floor(
@@ -61,6 +78,14 @@ export default class Deck {
     }
   }
 
+  /**
+   * デッキから1枚引いて返す
+   *
+   * @remarks
+   * カードが残っていない場合はundefinedを返す
+   *
+   * @returns デッキから引いたCardクラスのインスタンス。デッキが空の場合はundefined。
+   */
   drawOne(): Card | undefined {
     if (this.isEmpty()) {
       return undefined;
@@ -68,10 +93,18 @@ export default class Deck {
     return this.cardList.pop();
   }
 
+  /**
+   * デッキが空かどうかを判定する
+   * @returns {boolean} デッキが空の場合true
+   */
   isEmpty(): boolean {
     return this.cardList.length === 0;
   }
 
+  /**
+   * カードの残り枚数を返す
+   * @returns {number} デッキ内の残りカード枚数
+   */
   getSize(): number {
     return this.cardList.length;
   }
