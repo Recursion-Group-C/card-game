23f67ddaf4d344b4e8ebda1bda9f85c6e2389123
Yuki Ueno <y0uk10126@gmail.com>
2023-04-25 21:01:49 +0900

    refactor: ♻️ #53 ファイル名の変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,42 +28,42 @@ const Home = () => (
             description="簡単なギャンブルゲーム。カードの合計点数が21点を超えないないで、ディーラーより高い点数を取れば勝ちです。"
             url="/games/blackjack"
             imgPath="/images/thumbnailSample.png"
-            ruleUrl = "/rules/blackjackRule"
+            ruleUrl="/rules/blackjack"
           />
           <Card
             title="War"
             description="数字の大小で勝負するシンプルなゲーム。1枚だけカードを引き、引いたカードの数字が大きい方が勝ちです。"
             url="/games/war"
             imgPath="/images/thumbnailSample.png"
-            ruleUrl = "/rules/warRule"
+            ruleUrl="/rules/war"
           />
           <Card
             title="Speed"
             description="導け、机上の最速理論！場に出されたカードに繋がる数字を相手よりも早く出して手札をなくした方が勝ちです。"
             url="/games/speed"
             imgPath="/images/thumbnailSample.png"
-            ruleUrl = "/rules/speedRule"
+            ruleUrl="/rules/speed"
           />
           <Card
             title="Porker"
             description="5枚のカードで役を作ります！カードの交換は1回だけ。国内で知名度のある基本的なポーカーゲームです。"
             url="/games/porker"
             imgPath="/images/thumbnailSample.png"
-            ruleUrl = "/rules/pokerRule"
+            ruleUrl="/rules/poker"
           />
           <Card
             title="Texas Hold'em Porker"
             description="世界的に大流行のポーカーゲーム。自分の手札は2枚。場に出される最大5枚のカードから役を作り、勝負！"
             url="/games/holdem"
             imgPath="/images/thumbnailSample.png"
-            ruleUrl = "/rules/texasHoldEmRule"
+            ruleUrl="/rules/holdem"
           />
           <Card
             title="Rummy"
             description="麻雀+花札のようなゲームです。場に出されたカードへも「付け札」が行え、最初に手札をなくした人の勝ちです。"
             url="/games/rummy"
             imgPath="/images/thumbnailSample.png"
-            ruleUrl = "/rules/rummyRule"
+            ruleUrl="/rules/rummy"
           />
         </div>
       </div>
