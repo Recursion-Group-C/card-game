c342d135382053acecbbb1246648ffd936748701
Yuki Ueno <y0uk10126@gmail.com>
2023-04-25 20:59:10 +0900

    refactor: ♻️ #53 ゲームで共通な文言を固定値へ変更する

--- components/Rule.tsx
+++ components/Rule.tsx
@@ -1,87 +1,44 @@
-import Link from 'next/link';
-import handleTableCreate from './Table';
+type Props = {
+  gameTitle: string;
+  gameDescription: string;
+  ruleDescription: string;
+  peopleDescription: string;
+};
 
 const Rule = ({
-  title,
-  description,
-  ruleTitle,
+  gameTitle,
+  gameDescription,
   ruleDescription,
-  people,
-  peopleDescription,
-  glossary,
-  glossaryPrompt,
-  backUrl
-}: {
-  title: string;
-  description: string;
-  ruleTitle: string;
-  ruleDescription: string;
-  people: string;
-  peopleDescription: string;
-  glossary: string;
-  glossaryPrompt: string;
-  backUrl: string;
-}) => (
-  /* eslint-disable */
-  <>
-    <div className="flex flex-col gap-4">
-      <div className="chat chat-start">
-        <div className="chat-bubble chat-bubble-primary ">
-          {title}
-        </div>
+  peopleDescription
+}: Props) => (
+  <div className="mb-6 flex flex-col gap-4">
+    <div className="chat chat-start">
+      <div className="chat-bubble chat-bubble-primary">
+        {gameTitle}とはどのようなゲームですか？
       </div>
-      <div className="flex w-full flex-col border-opacity-50">
-        <div className="card rounded-box grid h-20 place-items-center bg-base-300">
-          {description}
-        </div>
-        <div className="divider"></div>
-        <div className="chat chat-end">
-          <div className="chat-bubble chat-bubble-info">
-            {ruleTitle}
-          </div>
-        </div>
-        <div className="h-50 card rounded-box mt-3 grid place-items-center bg-base-300">
-          {ruleDescription}
-        </div>
-      </div>
-
-      <div className="chat chat-start">
-        <div className="chat-bubble chat-bubble-primary ">
-          {people}
-        </div>
-      </div>
-      <div className="flex w-full flex-col border-opacity-50">
-        <div className="card rounded-box grid h-20 place-items-center bg-base-300">
-          {peopleDescription}
-        </div>
-        <div className="divider"></div>
-        <div className="chat chat-end">
-          <div className="chat-bubble chat-bubble-info">
-            {glossary}
-          </div>
-        </div>
-        <div className="h-50 card rounded-box mt-3 grid place-items-center bg-base-300">
-          {handleTableCreate(glossaryPrompt)}
-        </div>
-      </div>
-      <Link href={backUrl}>
-        <button
-          className="mt-5 h-12 w-64 rounded-full border-white bg-red-600 px-4 py-2 text-white hover:bg-red-500"
-          type="button"
-        >
-          <p className="text-xl">back</p>
-        </button>
-      </Link>
     </div>
-  </>
-);
-export default Rule;
+    <div className="chat chat-end">
+      <div className="chat-bubble">{gameDescription}</div>
+    </div>
 
-{/* <Link href={backUrl}>
-          <button
-            className="h-12 w-64 rounded-full border-white bg-red-600 px-4 py-2 text-white hover:bg-red-500"
-            type="button"
-          >
-            <p className="text-xl">back</p>
-          </button>
-        </Link> */}
+    <div className="chat chat-start">
+      <div className="chat-bubble chat-bubble-primary">
+        ゲームの流れを教えてください。
+      </div>
+    </div>
+    <div className="chat chat-end">
+      <div className="chat-bubble">{ruleDescription}</div>
+    </div>
+
+    <div className="chat chat-start">
+      <div className="chat-bubble chat-bubble-primary">
+        プレー人数を教えてください。
+      </div>
+    </div>
+    <div className="chat chat-end">
+      <div className="chat-bubble">{peopleDescription}</div>
+    </div>
+  </div>
+);
+
+export default Rule;
